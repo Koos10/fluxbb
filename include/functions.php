fab0179ec2a2d99dec904698f942c78c406cc55d
Koos <pampoen10@yahoo.com>
2014-02-03 16:26:07 +0200

    Now user post counts get decremented when deleting posts/topics. The post count therefore now reflects the user's true number of posts in the forum. This also prevents spammers from artificially increasing their post counts (by continuously posting and deleting their post).

--- include/functions.php
+++ include/functions.php
@@ -730,6 +730,8 @@ function delete_topic($topic_id)
 	// Make sure we have a list of post IDs
 	if ($post_ids != '')
 	{
+		decrease_post_counts($post_ids);
+
 		strip_search_index($post_ids);
 
 		// Delete posts in topic
@@ -744,7 +746,7 @@ function delete_topic($topic_id)
 //
 // Delete a single post
 //
-function delete_post($post_id, $topic_id)
+function delete_post($post_id, $topic_id, $poster_id)
 {
 	global $db;
 
@@ -754,6 +756,9 @@ function delete_post($post_id, $topic_id)
 
 	// Delete the post
 	$db->query('DELETE FROM '.$db->prefix.'posts WHERE id='.$post_id) or error('Unable to delete post', __FILE__, __LINE__, $db->error());
+
+	// Decrement user post count
+	$db->query('UPDATE '.$db->prefix.'users SET num_posts=num_posts-1 WHERE id='.$poster_id) or error('Unable to update user post count', __FILE__, __LINE__, $db->error());
 
 	strip_search_index($post_id);
 
@@ -2053,6 +2058,30 @@ function forum_is_writable($path)
 		@unlink($path);
 
 	return true;
+}
+
+
+//
+// Decrease user post counts (used before deleting posts)
+//
+function decrease_post_counts($post_ids)
+{
+		global $db;
+
+		// Count the post counts for each user to be subtracted
+		$user_posts = array();
+		$result = $db->query('SELECT poster_id FROM '.$db->prefix.'posts WHERE id IN('.$post_ids.')') or error('Unable to fetch posts', __FILE__, __LINE__, $db->error());
+		while ($row = $db->fetch_assoc($result))
+		{
+			if (!isset($user_posts[$row['poster_id']]))
+				$user_posts[$row['poster_id']] = 1;
+			else
+				++$user_posts[$row['poster_id']];
+		}
+
+		// Decrease the post counts
+		foreach($user_posts as $user_id => $subtract)
+			$db->query('UPDATE '.$db->prefix.'users SET num_posts=num_posts-'.$subtract.' WHERE id='.$user_id) or error('Unable to update user post count', __FILE__, __LINE__, $db->error());
 }
 
 
--- moderate.php
+++ moderate.php
@@ -96,6 +96,8 @@ if (isset($_GET['tid']))
 			if ($db->num_rows($result) != substr_count($posts, ',') + 1)
 				message($lang_common['Bad request'], false, '404 Not Found');
 
+			decrease_post_counts($posts);
+
 			// Delete the posts
 			$db->query('DELETE FROM '.$db->prefix.'posts WHERE id IN('.$posts.')') or error('Unable to delete posts', __FILE__, __LINE__, $db->error());
 
@@ -689,7 +691,11 @@ else if (isset($_POST['delete_topics']) || isset($_POST['delete_topics_comply'])
 
 		// We have to check that we actually have a list of post IDs since we could be deleting just a redirect topic
 		if ($post_ids != '')
+		{
+			decrease_post_counts($post_ids);
+
 			strip_search_index($post_ids);
+		}
 
 		// Delete posts
 		$db->query('DELETE FROM '.$db->prefix.'posts WHERE topic_id IN('.$topics.')') or error('Unable to delete posts', __FILE__, __LINE__, $db->error());
